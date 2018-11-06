b19f17681e7a65b6094d01678fe56aba1d974ec4
Sean von Bayern <seanvonbayern@gmail.com>
2018-11-05 17:00:25 -0800

    Initial commit

---
+++ design.js
@@ -0,0 +1,15 @@
+var color;
+var height;
+var width;
+
+// When size is submitted by the user:
+// 1. Store submission in vars
+// 2. Call makeGrid() and pass it the vars
+
+function makeGrid() {
+// Destroy any previous grid!
+// Use nested loops to:
+    // 1. Create [height] #pixelCanvas rows
+        // 2. Create [width] #pixelCanvas columns per row
+            // 3. Insert color function into column cells
+}
