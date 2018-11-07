185b09219dd9a8b6787d59a94a895757180b7646
Sean von Bayern <seanvonbayern@gmail.com>
2018-11-06 20:20:00 -0800

    Add function to create canvas

--- design.js
+++ design.js
@@ -1,15 +1,32 @@
-var color;
-var height;
-var width;
+// REMINDER: Use "const" for global and "let" for function variables
+// Udacity's JavaScript Style Guide says "var" isn't cool anymore
 
-// When size is submitted by the user:
-// 1. Store submission in vars
-// 2. Call makeGrid() and pass it the vars
+function createCanvas(canvasHeight, canvasWidth) {
+    // Remove previous canvas
+    pixelCanvas.innerHTML = "";
+    // Create [canvasHeight] new rows
+    for (let row = 1; row <= canvasHeight; row += 1) {
+        let newRow = document.createElement("tr");
+        pixelCanvas.appendChild(newRow);
+        // Create [canvasWidth] new columns
+        for (let column = 1; column <= canvasWidth; column += 1) {
+            let newColumn = document.createElement("td");
+            newRow.appendChild(newColumn);
+        }};
+    // TODO: Add color function to cells
+    // TODO: Add erase function to cells (?)
+};
 
-function makeGrid() {
-// Destroy any previous grid!
-// Use nested loops to:
-    // 1. Create [height] #pixelCanvas rows
-        // 2. Create [width] #pixelCanvas columns per row
-            // 3. Insert color function into column cells
-}
+sizePicker.addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Update size variables
+    let canvasHeight = inputHeight.value;
+    let canvasWidth = inputWidth.value;
+    console.log(
+        "Created a new canvas with " +
+        canvasWidth + "x" +
+        canvasHeight + "-cell dimensions!"
+        );
+    // Create new canvas
+    createCanvas(canvasHeight, canvasWidth);
+});
